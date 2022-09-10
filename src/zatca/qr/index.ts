95b8360fa2e80c9e4761ed8101c357263210f770
Wesam Alzahir <za.wessam@gmail.com>
2022-09-11 00:49:07 +0300

    Extract fields for QR

--- src/zatca/qr/index.ts
+++ src/zatca/qr/index.ts
@@ -1,11 +1,36 @@
 import { XMLDocument } from "../../parser";
+import { getInvoiceHash } from "../signing";
 
+
+interface QRParams {
+    invoice_xml: XMLDocument,
+    digital_signature: string,
+    public_key: Buffer,
+    certificate_signature: Buffer
+}
 
 /**
  * Generates QR for a given invoice. According to ZATCA (TODO: BUSSINESS TERM RULE)
  * @param invoice_xml XMLDocument.
+ * @param digital_signature String base64 encoded digital signature.
+ * @param public_key Buffer certificate public key.
+ * @param certificate_signature Buffer certificate signature. 
  * @returns String base64 encoded QR data.
  */
-export const generateQR = (invoice_xml: XMLDocument): string => {
+export const generateQR = ({invoice_xml, digital_signature, public_key, certificate_signature}: QRParams): string => {
+
+    // Hash 
+    const invoice_hash: string = getInvoiceHash(invoice_xml);
+
+    // Extract required tags
+    const seller_name = invoice_xml.get("Invoice/cac:AccountingSupplierParty/cac:Party/cac:PartyLegalEntity/cbc:RegistrationName")?.[0];
+    const VAT_number = invoice_xml.get("Invoice/cac:AccountingSupplierParty/cac:Party/cac:PartyTaxScheme/cbc:CompanyID")?.[0];
+    const invoice_total = invoice_xml.get("Invoice/cac:LegalMonetaryTotal/cbc:TaxInclusiveAmount")?.[0]["#text"];
+    const VAT_total = invoice_xml.get("Invoice/cac:TaxTotal")?.[0]["cbc:TaxAmount"]["#text"];
+    const issue_date = invoice_xml.get("Invoice/cbc:IssueDate")?.[0];
+    const issue_time = invoice_xml.get("Invoice/cbc:IssueTime")?.[0];
+    const invoice_type = invoice_xml.get("Invoice/cbc:InvoiceTypeCode")?.[0]["@_name"];
+    
+
     return "";
 }
