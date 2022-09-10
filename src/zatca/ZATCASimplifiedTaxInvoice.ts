e1de07b4eea0a7d0de4766ee08cae1acfe83899c
Wesam Alzahir <za.wessam@gmail.com>
2022-09-10 21:55:08 +0300

    purify invoice and gethash initial

--- src/zatca/ZATCASimplifiedTaxInvoice.ts
+++ src/zatca/ZATCASimplifiedTaxInvoice.ts
@@ -18,6 +18,29 @@ export class ZATCASimplifiedTaxInvoice {
             this.invoice_xml = new XMLDocument(DEFAULT_INVOICE);
         }
 
+        // TODO: Validate Invoice
+
+    }
+
+
+    /**
+     * Removes (UBLExtensions (Signing), Signature Envelope, and QR data) Elements. Then canonicalizes the XML to c14n.
+     * In Order to prep for hashing.
+     */
+    private getPureInvoice() {
+        const invoice_copy = new XMLDocument(this.invoice_xml.toString({no_header: false}));
+        invoice_copy.delete("Invoice/ext:UBLExtensions");
+        invoice_copy.delete("Invoice/cac:Signature");
+        invoice_copy.delete("Invoice/cac:AdditionalDocumentReference", {"cbc:ID": "QR"});
+
+        return invoice_copy;
+    }
+
+    getInvoiceHash(): string {
+        const pure_invoice: XMLDocument = this.getPureInvoice();
+        console.log(pure_invoice.toString({no_header: false}));
+        
+        return "";
     }
 
 
