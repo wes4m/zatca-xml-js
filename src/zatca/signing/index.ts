e8c7d1c90e6bd2014ef4a67c662cea3faa83be3a
Wesam Alzahir <za.wessam@gmail.com>
2022-09-10 22:36:09 +0300

    Adding getCertificate hash

--- src/zatca/signing/index.ts
+++ src/zatca/signing/index.ts
@@ -7,8 +7,8 @@ import { XMLDocument } from "../../parser";
 /**
  * Removes (UBLExtensions (Signing), Signature Envelope, and QR data) Elements. Then canonicalizes the XML to c14n.
  * In Order to prep for hashing.
- * @param invoice_xml XMLDocument
- * @returns purified Invoice XML string
+ * @param invoice_xml XMLDocument.
+ * @returns purified Invoice XML string.
  */
 export const getPureInvoiceString = (invoice_xml: XMLDocument): string => {
     const invoice_copy = new XMLDocument(invoice_xml.toString({no_header: false}));
@@ -27,11 +27,21 @@ export const getPureInvoiceString = (invoice_xml: XMLDocument): string => {
 }
 
 /**
- * Hashes Invoice
- * @param invoice_xml XMLDocument
- * @returns String Invoice hash encoded in base64
+ * Hashes Invoice according to ZATCA (TODO RULE NUMBER BUSSINESS TERM)
+ * @param invoice_xml XMLDocument.
+ * @returns String invoice hash encoded in base64.
  */
 export const getInvoiceHash = (invoice_xml: XMLDocument): string => {
     const pure_invoice_string: string = getPureInvoiceString(invoice_xml);
     return createHash("sha256").update(pure_invoice_string).digest('base64');
+}
+
+/**
+ * Hashes Certificate according to ZATCA (TODO RULE NUMBER BUSSINESS TERM)
+ * @param certificate_string base64 encoded certificate body string
+ * @returns String certificate hash encoded in base64.
+ */
+export const getCertificateHash = (certificate_string: string): string => {
+    const certificate_hash = Buffer.from(createHash("sha256").update(certificate_string).digest('hex')).toString("base64");
+    return certificate_hash;
 }
