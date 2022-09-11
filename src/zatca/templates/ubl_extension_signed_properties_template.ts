d6965984402a25fdb91ab84e529211524b4efbf1
Wesam Alzahir <za.wessam@gmail.com>
2022-09-11 15:31:56 +0300

    template fix to match exact spacing/format for hashing

--- src/zatca/templates/ubl_extension_signed_properties_template.ts
+++ src/zatca/templates/ubl_extension_signed_properties_template.ts
@@ -1,22 +1,20 @@
-const template = /* XML */`
-<xades:SignedProperties Id="xadesSignedProperties">
-    <xades:SignedSignatureProperties>
-        <xades:SigningTime>SET_SIGN_TIMESTAMP</xades:SigningTime>
-        <xades:SigningCertificate>
-            <xades:Cert>
-                <xades:CertDigest>
-                    <ds:DigestMethod Algorithm="http://www.w3.org/2001/04/xmlenc#sha256"/>
-                    <ds:DigestValue>SET_CERTIFICATE_HASH</ds:DigestValue>
-                </xades:CertDigest>
-                <xades:IssuerSerial>
-                    <ds:X509IssuerName>SET_CERTIFICATE_ISSUER</ds:X509IssuerName>
-                    <ds:X509SerialNumber>SET_CERTIFICATE_SERIAL_NUMBER</ds:X509SerialNumber>
-                </xades:IssuerSerial>
-            </xades:Cert>
-        </xades:SigningCertificate>
-    </xades:SignedSignatureProperties>
-</xades:SignedProperties>
-`;
+const template = /* XML */`<xades:SignedProperties xmlns:xades="http://uri.etsi.org/01903/v1.3.2#" Id="xadesSignedProperties">
+                                    <xades:SignedSignatureProperties>
+                                        <xades:SigningTime>SET_SIGN_TIMESTAMP</xades:SigningTime>
+                                        <xades:SigningCertificate>
+                                            <xades:Cert>
+                                                <xades:CertDigest>
+                                                    <ds:DigestMethod xmlns:ds="http://www.w3.org/2000/09/xmldsig#" Algorithm="http://www.w3.org/2001/04/xmlenc#sha256"/>
+                                                    <ds:DigestValue xmlns:ds="http://www.w3.org/2000/09/xmldsig#">SET_CERTIFICATE_HASH</ds:DigestValue>
+                                                </xades:CertDigest>
+                                                <xades:IssuerSerial>
+                                                    <ds:X509IssuerName xmlns:ds="http://www.w3.org/2000/09/xmldsig#">SET_CERTIFICATE_ISSUER</ds:X509IssuerName>
+                                                    <ds:X509SerialNumber xmlns:ds="http://www.w3.org/2000/09/xmldsig#">SET_CERTIFICATE_SERIAL_NUMBER</ds:X509SerialNumber>
+                                                </xades:IssuerSerial>
+                                            </xades:Cert>
+                                        </xades:SigningCertificate>
+                                    </xades:SignedSignatureProperties>
+                                </xades:SignedProperties>`;
 
 
 export default function populate(
